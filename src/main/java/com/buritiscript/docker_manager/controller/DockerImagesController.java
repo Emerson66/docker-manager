89a7493bc56796fcab4606633f071f0c1c112b5e
Emerson66 <emersonsinho66@gmail.com>
2024-09-21 20:58:48 -0400

    Criado classe servic

---
+++ src/main/java/com/buritiscript/docker_manager/controller/DockerImagesController.java
@@ -0,0 +1,32 @@
+package com.buritiscript.docker_manager.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.buritiscript.docker_manager.services.DockerService;
+import com.github.dockerjava.api.model.Image;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/images")
+public class DockerImagesController {
+    
+    private final DockerService dockerService;
+
+    public DockerImagesController(DockerService dockerService){
+        this.dockerService = dockerService;
+    }
+
+    @GetMapping("")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    public List<Image> listImages(){
+        return dockerService.listImages();
+    }
+}
